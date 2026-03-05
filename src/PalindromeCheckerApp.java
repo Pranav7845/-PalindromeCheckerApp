0e87a5c156ec96a60ba721dd3bd810a6e6bf6484
Pranav7845 <nnmguru@gmail.com>
2026-03-05 09:56:48 +0530

    UC2:Print a Hardcoded Palindrome Result

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,23 @@
 import java.util.*;
 public class PalindromeCheckerApp{
     public static void main(String[] args){
-        showWelcome(); //UC1
-        checkHardcodedPalindrome(); //UC2
-    }
-    //UC1 : Welcome Message
-    public static void showWelcome() {
-        System.out.println("Welcome to the Palindrome Checker Management");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully");
-    }
-
-    public static void checkHardcodedPalindrome {
-        String input = "madam";
-        boolean isPalindrome = true;
+            String input = "madam";
+            boolean isPalindrome = true;
 
 
-        // Loop till half of the string
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
+            // Loop till half of the string
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
             }
+
+            // Required Output Format
+            System.out.println("Input text: " + input);
+            System.out.println("Is it a Palindrome? : " + isPalindrome);
+
         }
-
-        // Required Output Format
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
-
     }
-}
+
+
